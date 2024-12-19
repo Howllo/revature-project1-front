ad9491fd792524022626b71280bd7859c7baecc3
Tony <allenr64y@ymail.com>
2024-12-19 08:02:55 -0600

    Work in progress sign up.

--- src/component/AuthContainer/SignUp.presentation.jsx
+++ src/component/AuthContainer/SignUp.presentation.jsx
@@ -4,7 +4,7 @@ import '@fontsource/inter/800.css';
 import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
 import {DatePicker} from "@mui/x-date-pickers";
-import {HorizontalSplit} from "@mui/icons-material";
+import {HorizontalRule, HorizontalSplit} from "@mui/icons-material";
 import {Link} from "react-router-dom";
 
 const SignUpPresentation = () => {
@@ -17,6 +17,7 @@ const SignUpPresentation = () => {
                 minHeight: '100vh',
                 display: 'grid',
                 gridTemplateColumns: '3fr 5fr',
+                backgroundColor: 'white',
             }}
         >
             <Box
@@ -50,7 +51,7 @@ const SignUpPresentation = () => {
                         marginTop: '10px',
                     }}
                 >
-                    We are glad to have you!
+                    We are glad to have you here!
                 </Typography>
             </Box>
             <Box
@@ -61,235 +62,259 @@ const SignUpPresentation = () => {
                     justifyContent: 'center',
                     backgroundColor: 'white',
                     paddingLeft: '60px',
+
                 }}
             >
-                <Typography
-                    variant="h5"
-                    fontFamily="Helvetica"
-                    sx={{
-                        fontSize: '12px',
-                        fontWeight: 800,
-                        color: 'rgb(102,102,102)'
-                    }}
-                >
-                    Step 1 of 3
-                </Typography>
-
-                <Typography
-                    variant="h4"
-                    fontFamily="Arial"
-                    sx={{
-                        fontSize: '20px',
-                        fontHeight: 'bold',
-                        fontWeight: 800,
-                        color: 'black'
-                    }}
-                >
-                    Create Account
-                </Typography>
-
                 <Box
                     sx={{
-                        marginTop: '40px',
+                        marginRight: '200px'
                     }}
                 >
                     <Typography
-                        variant="h4"
-                        fontFamily="Arial"
-                        sx={{
-                            fontSize: '15px',
-                            fontWeight: 600,
-                            color: 'rgb(66, 87, 108)'
-                        }}
-                    >
-                        Email
-                    </Typography>
-
-                    <OutlinedInput
-                        required
-                        id="outlined-required"
-                        placeholder="Enter your email address"
-                        variant="outlined"
-                        sx={{
-                            maxWidth: '450px',
-                            marginTop: '10px',
-                            width: 'calc(100% - 40px)',
-                        }}
-                        startAdornment={
-                            <InputAdornment position="start">
-                                <EmailIcon/>
-                            </InputAdornment>
-                        }
-                    />
-                </Box>
-
-
-                <Box>
-                    <Typography
-                        variant="h4"
-                        fontFamily="Arial"
-                        sx={{
-                            marginTop: '10px',
-                            fontSize: '15px',
-                            fontWeight: 600,
-                            color: 'rgb(66, 87, 108)'
-                        }}
-                    >
-                        Password
-                    </Typography>
-
-                    <OutlinedInput
-                        required
-                        type="password"
-                        id="outlined-required"
-                        placeholder="Enter your password"
-                        variant="outlined"
-                        sx={{
-                            maxWidth: '450px',
-                            marginTop: '10px',
-                            width: 'calc(100% - 40px)',
-                        }}
-                        startAdornment={
-                            <InputAdornment position="start">
-                                <LockIcon/>
-                            </InputAdornment>
-                        }
-                    />
-                </Box>
-
-                <Box>
-                    <Typography
-                        variant="h4"
-                        fontFamily="Arial"
-                        sx={{
-                            marginTop: '10px',
-                            fontSize: '15px',
-                            fontWeight: 600,
-                            color: 'rgb(66, 87, 108)'
-                        }}
-                    >Your birth date</Typography>
-
-                    <DatePicker
-                        format="YYYY/MM/DD"
-                        sx={{
-                            maxWidth: '450px',
-                            marginTop: '10px',
-                            width: 'calc(100% - 40px)',
-                            marginBottom: '20px',
-                        }}
-                    />
-                </Box>
-
-                <Box
-                    sx={{
-                        flexDirection: 'row',
-                        display: 'flex',
-                        marginTop: '-3px',
-                    }}
-                >
-                    <Typography
-                        variant="h4"
-                        fontFamily="Arial"
+                        variant="h5"
+                        fontFamily="Helvetica"
                         sx={{
                             fontSize: '12px',
-                            fontWeight: 600,
-                            color: 'rgb(66, 87, 108)'
+                            fontWeight: 800,
+                            color: 'rgb(102,102,102)'
                         }}
                     >
-                        By creating an account, you agree to our
+                        Step 1 of 3
                     </Typography>
-                    <Link to=''>
+
+                    <Typography
+                        variant="h4"
+                        fontFamily="Arial"
+                        sx={{
+                            fontSize: '20px',
+                            fontHeight: 'bold',
+                            fontWeight: 800,
+                            color: 'black'
+                        }}
+                    >
+                        Create Account
+                    </Typography>
+
+                    <Box
+                        sx={{
+                            marginTop: '40px',
+                        }}
+                    >
                         <Typography
                             variant="h4"
                             fontFamily="Arial"
                             sx={{
-                                textDecoration: 'underline',
-                                marginLeft: '3px',
-                                fontSize: '12px',
+                                fontSize: '15px',
                                 fontWeight: 600,
-                                color: 'rgb(25, 118, 210)'
+                                color: 'rgb(66, 87, 108)'
                             }}
                         >
-                            Terms of Service
+                            Email
                         </Typography>
-                    </Link>
 
-                    <Typography
-                        variant="h4"
-                        fontFamily="Arial"
-                        sx={{
-                            marginLeft: '3px',
-                            fontSize: '12px',
-                            fontWeight: 600,
-                            color: 'rgb(66, 87, 108)'
-                        }}
-                    >
-                        and
-                    </Typography>
+                        <OutlinedInput
+                            required
+                            type="email"
+                            id="outlined-required"
+                            placeholder="Enter your email address"
+                            variant="outlined"
+                            sx={{
+                                maxWidth: '450px',
+                                marginTop: '10px',
+                                width: '100%',
+                            }}
+                            startAdornment={
+                                <InputAdornment position="start">
+                                    <EmailIcon/>
+                                </InputAdornment>
+                            }
+                        />
+                    </Box>
 
-                    <Link to=''>
+
+                    <Box>
                         <Typography
                             variant="h4"
                             fontFamily="Arial"
                             sx={{
-                                textDecoration: 'underline',
+                                marginTop: '10px',
+                                fontSize: '15px',
+                                fontWeight: 600,
+                                color: 'rgb(66, 87, 108)'
+                            }}
+                        >
+                            Password
+                        </Typography>
+
+                        <OutlinedInput
+                            required
+                            type="password"
+                            id="outlined-required"
+                            placeholder="Enter your password"
+                            variant="outlined"
+                            sx={{
+                                maxWidth: '450px',
+                                marginTop: '10px',
+                                width: 'calc(100% - 40px)',
+                            }}
+                            startAdornment={
+                                <InputAdornment position="start">
+                                    <LockIcon/>
+                                </InputAdornment>
+                            }
+                        />
+                    </Box>
+
+                    <Box>
+                        <Typography
+                            variant="h4"
+                            fontFamily="Arial"
+                            sx={{
+                                marginTop: '10px',
+                                fontSize: '15px',
+                                fontWeight: 600,
+                                color: 'rgb(66, 87, 108)'
+                            }}
+                        >Your birth date</Typography>
+
+                        <DatePicker
+                            format="YYYY/MM/DD"
+                            sx={{
+                                maxWidth: '450px',
+                                marginTop: '10px',
+                                width: 'calc(100% - 40px)',
+                                marginBottom: '20px',
+                            }}
+                        />
+                    </Box>
+
+                    <Box
+                        sx={{
+                            flexDirection: 'row',
+                            display: 'flex',
+                            marginTop: '-3px',
+                        }}
+                    >
+                        <Typography
+                            variant="h4"
+                            fontFamily="Arial"
+                            sx={{
+                                fontSize: '12px',
+                                fontWeight: 600,
+                                color: 'rgb(66, 87, 108)'
+                            }}
+                        >
+                            By creating an account, you agree to our
+                        </Typography>
+                        <Link to=''>
+                            <Typography
+                                variant="h4"
+                                fontFamily="Arial"
+                                sx={{
+                                    textDecoration: 'underline',
+                                    marginLeft: '3px',
+                                    fontSize: '12px',
+                                    fontWeight: 600,
+                                    color: 'rgb(25, 118, 210)'
+                                }}
+                            >
+                                Terms of Service
+                            </Typography>
+                        </Link>
+
+                        <Typography
+                            variant="h4"
+                            fontFamily="Arial"
+                            sx={{
                                 marginLeft: '3px',
                                 fontSize: '12px',
                                 fontWeight: 600,
-                                color: 'rgb(25, 118, 210)'
+                                color: 'rgb(66, 87, 108)'
                             }}
                         >
-                            Privacy Policy
+                            and
                         </Typography>
-                    </Link>
-                    <Typography
-                        variant="h4"
-                        fontFamily="Arial"
-                        sx={{
-                            marginLeft: '1px',
-                            fontSize: '12px',
-                            fontWeight: 600,
-                            color: 'rgb(56,155,253)'
-                        }}
-                    >
-                        .
-                    </Typography>
-                </Box>
 
-                <Box
-                    sx={{
-                        marginTop: '40px',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        width: '100%',
-                    }}
-                >
-                    <Button
-                        variant="contained"
-                        sx={{
-                            backgroundColor: 'rgb(212,217,225)',
-                            color: 'rgb(66, 87, 108)',
-                            fontWeight: 600,
-                            textTransform: 'capitalize',
-                        }}
+                        <Link to=''>
+                            <Typography
+                                variant="h4"
+                                fontFamily="Arial"
+                                sx={{
+                                    textDecoration: 'underline',
+                                    marginLeft: '3px',
+                                    fontSize: '12px',
+                                    fontWeight: 600,
+                                    color: 'rgb(25, 118, 210)'
+                                }}
+                            >
+                                Privacy Policy
+                            </Typography>
+                        </Link>
+                        <Typography
+                            variant="h4"
+                            fontFamily="Arial"
+                            sx={{
+                                marginLeft: '1px',
+                                fontSize: '12px',
+                                fontWeight: 600,
+                                color: 'rgb(56,155,253)'
+                            }}
                         >
-                        Back
-                    </Button>
+                            .
+                        </Typography>
+                    </Box>
 
-                    <Button
-                        variant="contained"
+                    <Box
                         sx={{
-                            backgroundColor: 'rgb(56,155,253)',
-                            color: 'white',
-                            fontWeight: 600,
-                            textTransform: 'capitalize',
-                            marginRight: '30vw',
+                            maxWidth: '450px',
+                            marginTop: '40px',
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            width: '100%',
                         }}
                     >
-                        Next
-                    </Button>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                backgroundColor: 'rgb(212,217,225)',
+                                color: 'rgb(66, 87, 108)',
+                                fontWeight: 600,
+                                textTransform: 'capitalize',
+                            }}
+                            >
+                            Back
+                        </Button>
+
+                        <Button
+                            variant="contained"
+                            sx={{
+                                backgroundColor: 'rgb(56,155,253)',
+                                color: 'white',
+                                fontWeight: 600,
+                                textTransform: 'capitalize',
+                            }}
+                        >
+                            Next
+                        </Button>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            marginTop: '10px',
+                            maxWidth: '450px',
+                        }}
+                    >
+                        <HorizontalRule
+                            sx={{
+                                color: 'rgb(212,217,225)',
+                                width: '100%',
+                                height: '1px',
+                                backgroundColor: 'rgb(212,217,225)',
+                            }}
+                        />
+                    </Box>
                 </Box>
             </Box>
         </Grid2>
