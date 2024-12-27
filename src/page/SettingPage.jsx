283684db6d1ea9b2b635e67f62acb903ce7c95ae
Tony <allenr64y@ymail.com>
2024-12-27 15:50:50 -0600

    Added changing display name.

--- src/page/SettingPage.jsx
+++ src/page/SettingPage.jsx
@@ -1,9 +1,72 @@
-﻿import {Box} from "@mui/material";
+﻿import {Box, Button, InputAdornment, OutlinedInput} from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import {useState} from "react";
+import {projectApi} from "../util/axios.js";
 
 const SettingsPage = () => {
+    const [displayName, setDisplayName] = useState()
+
+    const changeName = async (username) => {
+        if(displayName === undefined) {
+            console.log(`This username was: ${displayName}`);
+            return [];
+        }
+
+        try {
+            const response = await projectApi.get(`/user/${username}/display_name`, {
+                    displayName: displayName
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+            return response.data;
+        } catch (e) {
+            console.error('Error getting search results for user: ', e.status);
+        }
+    }
+
+    const handleDisplayNameChange = (e) => {
+        setDisplayName(e.target.value);
+    }
+
+    const handleDisplayChange = async () => {
+        await changeName(displayName);
+    }
+
     return (
         <Box>
+            <OutlinedInput
+                required
+                type="text"
+                className="text"
+                value={displayName}
+                onChange={(e) => handleDisplayNameChange(e)}
+                placeholder="Enter new display name"
+                sx={{
+                    maxWidth: '450px',
+                    marginTop: '10px',
+                    width: '100%',
+                }}
+                startAdornment={
+                    <InputAdornment position="start">
+                        <EmailIcon />
+                    </InputAdornment>
+                }
+            />
 
+            <Button
+                variant="contained"
+                onClick={handleDisplayChange}
+
+                sx={{
+                    marginTop: '10px',
+                }}
+            >
+                Change Name
+            </Button>
         </Box>
     )
 }
